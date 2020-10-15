8b8a86afa3229853e68d722391c00c9f19c1fc88
abhilash <abhilash.kamble376@gmail.com>
2020-10-15 12:32:57 +0530

    http

---
+++ day18/app2/src/app/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email = ''
+  password = ''
+
+  // class attribute
+  httpClient: HttpClient
+
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient
+  }
+
+  ngOnInit(): void {
+  }
+
+  onLogin() {
+    console.log(`email = ${this.email}`)
+    console.log(`password = ${this.password}`)
+
+    const body = {
+      email: this.email,
+      password: this.password
+    }
+
+    const url = 'http://localhost:3000/user/signin'
+    const request = this.httpClient.post(url, body)
+    request.subscribe(response => {
+      console.log(response)
+      if (response['status'] == 'success') {
+        const data = response['data']
+        const firstName = data['firstName']
+        const lastName = data['lastName']
+        alert(`welcome ${firstName} ${lastName}`)
+      } else {
+        alert('invalid email or password')
+      }
+    })
+  }
+
+  onCancel() {
+
+  }
+
+}
---
+++ day18/app2/src/app/product-list/product-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {HttpClient} from '@angular/common/http'
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products = []
+
+  // member attribute
+  httpClient: HttpClient
+
+  constructor(
+    httpClient: HttpClient) {
+
+      this.httpClient = httpClient
+  }
+
+  ngOnInit(): void {
+  }
+
+  loadProducts() {
+    const url = 'http://localhost:3000/product'
+
+    // send a get request
+    const request = this.httpClient.get(url)
+    
+    // get the response
+    request.subscribe(response => {
+      console.log(`response`)
+      console.log(response)
+      if (response['status'] == 'success') {
+        this.products = response['data']
+      }
+    })
+
+  }
+
+}
