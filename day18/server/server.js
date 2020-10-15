fffba93c3e92fe2b02ae44a57735b2818d1cd6af
abhilash <abhilash.kamble376@gmail.com>
2020-10-15 11:22:40 +0530

    server

--- day18/server/server.js
+++ day18/server/server.js
@@ -1,10 +1,22 @@
-const express=require('express')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app=express()
+const app = express()
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next();
+});
+
+app.use(bodyParser.json())
 
 
-app.listen(3000,'0.0.0.0',()=>{
-    console.log('server started')
+const userRouter = require('./routes/user')
+const productRouter = require('./routes/product')
+
+app.use('/user', userRouter)
+app.use('/product', productRouter)
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('server started on port 3000')
 })
-
-
