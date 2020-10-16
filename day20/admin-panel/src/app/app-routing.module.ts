b1f7a6649f9688beed43ad1d627acf9665a59091
abhilash <abhilash.kamble376@gmail.com>
2020-10-16 12:26:10 +0530

    user

--- day20/admin-panel/src/app/app-routing.module.ts
+++ day20/admin-panel/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './../../../../day18/app1/src/app/login/login.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { ProductListComponent } from './../../../../day18/app1/src/app/product-list/product-list.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashbord',component:DashboardComponent},
+  {path:'user-list',component:UserListComponent},
+  {path:'product-list',component:ProductListComponent},
+  {path:'order-list',component:OrderListComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
--- day20/admin-panel/src/app/app.module.ts
+++ day20/admin-panel/src/app/app.module.ts
@@ -1,18 +1,48 @@
+import { OrderService } from './order.service';
+import { UserService } from './user.service';
+import { AdminService } from './admin.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { OrderListComponent } from './order-list/order-list.component';
+
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProductService } from './product.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    UserListComponent,
+    ProductListComponent,
+    OrderListComponent,
+    LoginComponent,
+    SignupComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AdminService,
+    ProductService,
+    UserService,
+    OrderService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
