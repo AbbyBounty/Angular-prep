610a2c01da6206eed056c12b95be170c32a667b9
abhilash <abhilash.kamble376@gmail.com>
2020-10-19 13:17:08 +0530

    user-module

--- day20/user-portal/src/app/user/user-routing.module.ts
+++ day20/user-portal/src/app/user/user-routing.module.ts
@@ -1,7 +1,18 @@
+import { AddressAddComponent } from './address-add/address-add.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AddressListComponent } from './address-list/address-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent}
+  {path:'signup',component:SignupComponent}
+  {path:'profile',component:ProfileComponent}
+  {path:'address-add',component:AddressAddComponent}
+  {path:'address-list',component:AddressListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
