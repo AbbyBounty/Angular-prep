58c38a034877e0252e1cba8b32415a275684aa0c
abhilash <abhilash.kamble376@gmail.com>
2020-10-19 13:37:24 +0530

    order

--- day20/user-portal/src/app/order/order-routing.module.ts
+++ day20/user-portal/src/app/order/order-routing.module.ts
@@ -1,7 +1,14 @@
+import { OrderHistoryComponent } from './order-history/order-history.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PreviewComponent } from './preview/preview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'preview',component:PreviewComponent},
+  {path:'order-history',component:OrderHistoryComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
